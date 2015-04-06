93216efbb6ba1a2d70a5d48089fc12267e3f134b
Alex Egg <eggie5@gmail.com>
2015-04-06 16:10:19 -0700

    missing file

--- app/controllers/devise/approvals_controller.rb
+++ app/controllers/devise/approvals_controller.rb
@@ -1,4 +1,10 @@
-class Devise::ApprovalsController < DeviseController
+class Developers::Devise::ApprovalsController < DeviseController
+
+  # GET /resource/confirmation/new
+  def new
+    self.resource = resource_class.new
+  end
+
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
     self.resource = resource_class.approve_by_token(params[:approval_token])
@@ -11,4 +17,19 @@ class Devise::ApprovalsController < DeviseController
       respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
   end
+
+  protected
+
+    # The path used after confirmation.
+    def after_approval_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
+      end
+    end
+
+    def translation_scope
+      'devise.approvals'
+    end
 end
